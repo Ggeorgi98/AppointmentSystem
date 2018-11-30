cd51aead64243998d94bc0eff5c2ff4fc3ceccaf
Georgi98 <georgigerdjikov033@gmail.com>
2018-11-30 20:44:28 +0200

    Add DbContext Factor

---
+++ AppointmentSystem.Data/Context/AppointmentSystemDbFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppointmentSystem.Data.Context
+{
+    public class AppointmentSystemDbFactory : IDesignTimeDbContextFactory<AppointmentSystemContext>
+    {
+        public AppointmentSystemContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppointmentSystemContext>();
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=AppointmentSystemDB;Trusted_Connection=True;Integrated Security=True");
+
+            return new AppointmentSystemContext(optionsBuilder.Options);
+        }
+    }
+}
